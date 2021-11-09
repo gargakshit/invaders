15dcb038d38dc18e11641d41a71084cccb099169
Akshit Garg <garg.akshit@gmail.com>
2021-11-09 23:10:24 +0530

    feat: initial emscripten support
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
-#include <SDL_events.h>
-#include <SDL_keycode.h>
-#include <iostream>
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#ifndef __EMSCRIPTEN_PTHREADS__
+#define SDL_THREADS_DISABLED 1
+#pragma message("Building for emscripten without SDL threads")
+#endif
+#endif
+
 #include <stdint.h>
+
+#include <iostream>
 
 // Dear Imgui
 #include <imgui.h>
@@ -16,9 +23,8 @@
 #include <SDL2/SDL_opengl.h>
 #endif
 
-#include "config.h"
-
 #include "bus.hpp"
+#include "config.h"
 #include "font.h"
 
 #if defined(_WIN32) || defined(_WIN64)
@@ -31,21 +37,180 @@ void initializePlatform() {
 void initializePlatform() {}
 #endif
 
+struct RenderLoopArgs {
+  bool &done;
+  invaders::Bus &bus;
+  SDL_Window *window;
+  bool &paused;
+  unsigned int &lastPartialFrame;
+  GLubyte *displayFramebuffer;
+  GLuint displayTexture;
+  ImGuiIO &io;
+  int *displayScale;
+};
+
+static inline void renderLoop(void *argsRaw) {
+  auto args = static_cast<RenderLoopArgs *>(argsRaw);
+
+  auto &done = args->done;
+  auto &bus = args->bus;
+  auto window = args->window;
+  auto &paused = args->paused;
+  auto &lastPartialFrame = args->lastPartialFrame;
+  auto displayFramebuffer = args->displayFramebuffer;
+  auto displayTexture = args->displayTexture;
+  auto &io = args->io;
+  auto displayScale = args->displayScale;
+
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    ImGui_ImplSDL2_ProcessEvent(&event);
+
+    if (event.type == SDL_QUIT) {
+      done = true;
+    }
+
+    if (event.type == SDL_KEYUP || event.type == SDL_KEYDOWN) {
+      auto p = event.type == SDL_KEYDOWN;
+
+      switch (event.key.keysym.sym) {
+      case SDLK_LEFT: bus.SetKeyboardState(invaders::P1_LEFT, p); break;
+      case SDLK_RIGHT: bus.SetKeyboardState(invaders::P1_RIGHT, p); break;
+      case SDLK_c: bus.SetKeyboardState(invaders::COIN, p); break;
+      case SDLK_SPACE: bus.SetKeyboardState(invaders::P1_FIRE, p); break;
+      case SDLK_1: bus.SetKeyboardState(invaders::P1_START, p); break;
+      }
+    }
+
+    if (event.type == SDL_WINDOWEVENT &&
+        event.window.event == SDL_WINDOWEVENT_CLOSE &&
+        event.window.windowID == SDL_GetWindowID(window)) {
+      done = true;
+    }
+  }
+
+  if (!paused) {
+    // Timers
+    auto now = SDL_GetTicks();
+
+    // Fire approximately every 16 milliseconds (will try to squeeze 60 FPS,
+    // or otherwise will run slower)
+    auto delta = now - lastPartialFrame;
+
+    if (delta >= 16) {
+      for (int i = 0; i < 16'500; i++) {
+        bus.TickCPU();
+      }
+      bus.cpu.Interrupt(1);
+      for (int i = 0; i < 16'500; i++) {
+        bus.TickCPU();
+      }
+      bus.cpu.Interrupt(2);
+      lastPartialFrame = now;
+    }
+
+    for (unsigned int x = 0; x < 224; ++x) {
+      for (unsigned int y = 0; y < 32; ++y) {
+        auto byte = bus.mem[0x2400 + ((x * 32) + y)];
+        for (unsigned int bit = 0; bit < 8; ++bit) {
+          auto i = (x + ((255 - ((y * 8) + bit)) * 224)) * 3;
+
+          if ((byte & (1 << bit)) == 0) {
+            // Black
+            displayFramebuffer[i] = 0x00;
+            displayFramebuffer[i + 1] = 0x00;
+            displayFramebuffer[i + 2] = 0x00;
+          } else if (y > 25 && y < 28) {
+            // Red
+            displayFramebuffer[i] = 0xff;
+            displayFramebuffer[i + 1] = 0x00;
+            displayFramebuffer[i + 2] = 0x00;
+          } else if (y < 10) {
+            // Green
+            displayFramebuffer[i] = 0x00;
+            displayFramebuffer[i + 1] = 0xff;
+            displayFramebuffer[i + 2] = 0x00;
+          } else {
+            // White
+            displayFramebuffer[i] = 0xff;
+            displayFramebuffer[i + 1] = 0xff;
+            displayFramebuffer[i + 2] = 0xff;
+          }
+        }
+      }
+    }
+
+    glBindTexture(GL_TEXTURE_2D, displayTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 224, 256, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, displayFramebuffer);
+  }
+
+  // Start the Dear ImGui frame
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplSDL2_NewFrame();
+  ImGui::NewFrame();
+
+  {
+    ImGui::Begin("General");
+    ImGui::Text("Framerate: %f", io.Framerate);
+    if (ImGui::Button(paused ? "Resume" : "Pause")) {
+      paused = !paused;
+    }
+    ImGui::InputInt("Display Scale", displayScale, 1, 1);
+
+    if (*displayScale < 1) {
+      *displayScale = 1;
+    } else if (*displayScale > 8) {
+      *displayScale = 8;
+    }
+
+    ImGui::End();
+  }
+
+  {
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+    ImGui::Begin("Display", NULL,
+                 ImGuiWindowFlags_AlwaysAutoResize |
+                     ImGuiWindowFlags_NoDecoration);
+    ImGui::Image((void *)(intptr_t)displayTexture,
+                 ImVec2(224 * *displayScale, 256 * *displayScale));
+    ImGui::End();
+    ImGui::PopStyleVar();
+  }
+
+  ImGui::Render();
+
+  glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+  SDL_GL_SwapWindow(window);
+}
+
 int main(int argc, char **args) {
+  // Only use the arguments when not running on emscripten
+#ifndef __EMSCRIPTEN__
   if (argc < 1) {
     std::cout << "No invaders ROM files specified. Aborting..." << std::endl;
     return 1;
   }
+#endif
 
   invaders::Bus bus;
   bus.Reset();
 
+#ifdef __EMSCRIPTEN__
+  if (!bus.LoadFileAt("space-invaders.rom", 0x0000)) {
+#else
   if (!bus.LoadFileAt(args[1], 0x0000)) {
+#endif
     std::cerr << "Unable to start the emulator";
     return -1;
   }
 
-  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     std::cerr << "SDL Error: " << SDL_GetError() << std::endl;
     return -1;
   }
@@ -71,12 +236,12 @@ int main(int argc, char **args) {
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 #else
-  // GL 3.0 + GLSL 130
-  const char *glsl_version = "#version 130";
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+// GL 3.0 + GLSL 130
+const char *glsl_version = "#version 130";
+SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #endif
 
   // Create a window
@@ -85,13 +250,12 @@ int main(int argc, char **args) {
   SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
   auto window_flags =
       (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE |
-                        SDL_WINDOW_ALLOW_HIGHDPI);
+                        SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_MAXIMIZED);
   auto *window =
-      SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+      SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_UNDEFINED,
+                       SDL_WINDOWPOS_UNDEFINED, 1280, 720, window_flags);
   auto gl_context = SDL_GL_CreateContext(window);
   SDL_GL_MakeCurrent(window, gl_context);
-  SDL_GL_SetSwapInterval(1); // Enable vsync
 
   // Setup Dear ImGui context
   IMGUI_CHECKVERSION();
@@ -146,132 +310,27 @@ int main(int argc, char **args) {
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 224, 256, 0, GL_RGB, GL_UNSIGNED_BYTE,
                displayFramebuffer);
 
+  // clang-format off
+  auto loopArgs = RenderLoopArgs{
+    done,
+    bus,
+    window,
+    paused,
+    lastPartialFrame,
+    displayFramebuffer,
+    displayTexture,
+    io,
+    &displayScale
+  };
+  // clang-format on
+
+#ifdef __EMSCRIPTEN__
+  emscripten_set_main_loop_arg(&renderLoop, &loopArgs, 0, 1);
+  SDL_GL_SetSwapInterval(1); // Enable vsync
+#else
+  SDL_GL_SetSwapInterval(1); // Enable vsync
   while (!done) {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-      ImGui_ImplSDL2_ProcessEvent(&event);
-
-      if (event.type == SDL_QUIT) {
-        done = true;
-      }
-
-      if (event.type == SDL_KEYUP || event.type == SDL_KEYDOWN) {
-        auto p = event.type == SDL_KEYDOWN;
-
-        switch (event.key.keysym.sym) {
-        case SDLK_LEFT: bus.SetKeyboardState(invaders::P1_LEFT, p); break;
-        case SDLK_RIGHT: bus.SetKeyboardState(invaders::P1_RIGHT, p); break;
-        case SDLK_c: bus.SetKeyboardState(invaders::COIN, p); break;
-        case SDLK_SPACE: bus.SetKeyboardState(invaders::P1_FIRE, p); break;
-        case SDLK_1: bus.SetKeyboardState(invaders::P1_START, p); break;
-        }
-      }
-
-      if (event.type == SDL_WINDOWEVENT &&
-          event.window.event == SDL_WINDOWEVENT_CLOSE &&
-          event.window.windowID == SDL_GetWindowID(window)) {
-        done = true;
-      }
-    }
-
-    if (!paused) {
-      // Timers
-      auto now = SDL_GetTicks();
-
-      // Fire approximately every 16 milliseconds (will try to squeeze 60 FPS,
-      // or otherwise will run slower)
-      auto delta = now - lastPartialFrame;
-
-      if (delta >= 16) {
-        for (int i = 0; i < 16'500; i++) {
-          bus.TickCPU();
-        }
-        bus.cpu.Interrupt(1);
-        for (int i = 0; i < 16'500; i++) {
-          bus.TickCPU();
-        }
-        bus.cpu.Interrupt(2);
-        lastPartialFrame = now;
-      }
-
-      for (unsigned int x = 0; x < 224; ++x) {
-        for (unsigned int y = 0; y < 32; ++y) {
-          auto byte = bus.mem[vramStart + ((x * 32) + y)];
-          for (unsigned int bit = 0; bit < 8; ++bit) {
-            auto i = (x + ((255 - ((y * 8) + bit)) * 224)) * 3;
-
-            if ((byte & (1 << bit)) == 0) {
-              // Black
-              displayFramebuffer[i] = 0x00;
-              displayFramebuffer[i + 1] = 0x00;
-              displayFramebuffer[i + 2] = 0x00;
-            } else if (y > 25 && y < 28) {
-              // Red
-              displayFramebuffer[i] = 0xff;
-              displayFramebuffer[i + 1] = 0x00;
-              displayFramebuffer[i + 2] = 0x00;
-            } else if (y < 10) {
-              // Green
-              displayFramebuffer[i] = 0x00;
-              displayFramebuffer[i + 1] = 0xff;
-              displayFramebuffer[i + 2] = 0x00;
-            } else {
-              // White
-              displayFramebuffer[i] = 0xff;
-              displayFramebuffer[i + 1] = 0xff;
-              displayFramebuffer[i + 2] = 0xff;
-            }
-          }
-        }
-      }
-
-      glBindTexture(GL_TEXTURE_2D, displayTexture);
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 224, 256, 0, GL_RGB,
-                   GL_UNSIGNED_BYTE, displayFramebuffer);
-    }
-
-    // Start the Dear ImGui frame
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplSDL2_NewFrame();
-    ImGui::NewFrame();
-
-    {
-      ImGui::Begin("General");
-      ImGui::Text("Framerate: %f", io.Framerate);
-      if (ImGui::Button(paused ? "Resume" : "Pause")) {
-        paused = !paused;
-      }
-      ImGui::InputInt("Display Scale", &displayScale, 1, 1);
-
-      if (displayScale < 1) {
-        displayScale = 1;
-      } else if (displayScale > 8) {
-        displayScale = 8;
-      }
-
-      ImGui::End();
-    }
-
-    {
-      ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
-      ImGui::Begin("Display", NULL,
-                   ImGuiWindowFlags_AlwaysAutoResize |
-                       ImGuiWindowFlags_NoDecoration);
-      ImGui::Image((void *)(intptr_t)displayTexture,
-                   ImVec2(224 * displayScale, 256 * displayScale));
-      ImGui::End();
-      ImGui::PopStyleVar();
-    }
-
-    ImGui::Render();
-
-    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-    SDL_GL_SwapWindow(window);
+    renderLoop(&loopArgs);
   }
 
   // Cleanup
@@ -282,6 +341,7 @@ int main(int argc, char **args) {
   SDL_GL_DeleteContext(gl_context);
   SDL_DestroyWindow(window);
   SDL_Quit();
+#endif
 
   return 0;
 }
